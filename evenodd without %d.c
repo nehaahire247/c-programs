7e945c2fbd0820b7bfdd76a96951056d96bed01a
nehaahire247 <80942072+nehaahire247@users.noreply.github.com>
2021-03-25 13:23:32 +0530

    Add files via upload

---
+++ evenodd without %d.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main()
+{float d,r,q;//d=dividend,divisor=2,quotient=d/2,r=remainder;
+printf("progrm for even odd no");
+scanf("%f",&d);
+q=2/d;
+r=d-2*q;}
