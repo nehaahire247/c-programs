996ff97e5b664cd164417e277a2a121c770c0d46
nehaahire247 <80942072+nehaahire247@users.noreply.github.com>
2021-03-25 13:22:05 +0530

    Add files via upload

---
+++ ASCII value prog clg assign 4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{char a;
+printf("display ASCII value of character");
+printf("\n enter single character ");
+scanf("%c",&a);
+printf("\n ASCII VALUE OF %c=%d",a,a);
+}
