e037d19fc85e3fb649c171831eefd3e212e5d019
nehaahire247 <80942072+nehaahire247@users.noreply.github.com>
2021-03-25 22:39:18 +0530

    Add files via upload

---
+++ display natural no.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void displayno(int ino1)
+int main()
+{int ino1;
+printf("enter integer");
+scanf("%d",&ino1);
+void displayno(int ino1);
+return 0;
+}
+void diplayno( int ino1)
+{int i;
+for(i=1;i<=ino1;i++)
+{printf("%d \t",i);
+}
+}
