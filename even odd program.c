1b56df31446ef2f14de7aa92ddabca8e04605d8a
nehaahire247 <80942072+nehaahire247@users.noreply.github.com>
2021-03-25 11:33:25 +0530

    Add files via upload

---
+++ even odd program.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main()
+{
+int a;
+printf("program to check no even or odd");
+scanf("%d",&a);
+if(a%2==0)
+{printf(" given no is even no");}
+else
+{printf("given no is odd no");
+}
+}
