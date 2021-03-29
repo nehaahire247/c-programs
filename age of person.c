6794892d386c40baa4ba032c8f5f9758435739c2
nehaahire247 <80942072+nehaahire247@users.noreply.github.com>
2021-03-29 19:24:34 +0530

    Add files via upload

---
+++ age of person.c
@@ -0,0 +1,12 @@
+//programe to display our age by entering birth year//
+#include<conio.h>
+#include<stdio.h>
+#include<math.h>
+int main()
+{int y,diff;
+printf("programe to display age by birth year");
+printf("\n enter ur birth year=");
+scanf("%d",&y);
+diff=2021-y;
+printf("your current age is %d",diff);
+}
