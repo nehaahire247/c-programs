6e25caaa22f155488d0c0128b26594d8af96d1c1
nehaahire247 <80942072+nehaahire247@users.noreply.github.com>
2021-05-25 00:15:27 +0530

    Add files via upload

---
+++ no of even odd no in array 6.2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+int evenodd (int a,int n);
+int main()
+{int i,n,a[50];
+printf("programe to display total no of even and odd no \n enter how many no u r going to enter:: ");
+scanf("%d",&n);
+printf("enter the numbers::");
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+int evenodd (a,n)
+printf("d,%d",e,o);
+}
+int evenodd (int a,int n)
+{int* e,int * o;
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2==0)
+        {
+            e++;
+        }
+        else o++;
+    }
+
+
+}
